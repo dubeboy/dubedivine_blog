4863bf5a569a3c933f7673e2ffed641a05af2158
Divine Dube <216004854@student.uj.ac.za>
2021-08-13 05:59:48 +0400

    done

--- pages/about.js
+++ pages/about.js
@@ -1,20 +1,22 @@
 import styles from "../styles/About.module.css";
 import {server} from "../config";
+import ReactMarkdown from "react-markdown";
 
-export default function About({ blogs }) {
+export default function About({ author }) {
     return (
         <div className="mainContainer">
-            <p className={styles.aboutHeading}>About Divine</p>
+            <p className="title">About {author.divine}</p>
+            <ReactMarkdown>{author.about}</ReactMarkdown>
         </div>
     )
 }
 
 export const getStaticProps = async () => {
-    const res = await fetch(`${server}/blogs`)
-    const blogs = await res.json()
+    const res = await fetch(`${server}/author`)
+    const author = await res.json()
     return {
         props: {
-            blogs
+            author
         }
     }
 }
