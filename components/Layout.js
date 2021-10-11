40855432cbb68c1c02f1ddf716dc06bf5416af1d
Divine Dube <216004854@student.uj.ac.za>
2021-10-11 23:42:20 +0400

    Tmp fix mobile code not scrolling

--- components/Layout.js
+++ components/Layout.js
@@ -13,6 +13,7 @@ const Layout = ({children, title, metaDescription }) => {
         <>
             <Head>
                 <title>{ title }</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="description" content={metaDescription} charSet="utf-8" />
                 <link rel="canonical" href={`https://dubedivine.com${asPath}`} />
                 <link rel="icon" href="/favicon.ico" />
